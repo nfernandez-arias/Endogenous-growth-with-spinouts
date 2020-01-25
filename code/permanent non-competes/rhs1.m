de0d045a1bc08130a743bdb4e898b15b8d3ac03e
Nicolas <nfernand@princeton.edu>
2020-01-25 14:11:39 -0500

    deleted some deprecated files

--- code/permanent non-competes/rhs1.m
+++
@@ -1,8 +0,0 @@
-function out = rhs1(z,pa,pm,ig,V0,Vq,Vm,zE,i_q,i_m)
-
-    out = (-ig.w0(i_q,i_m) * z + pm.nu * Vm(i_q,i_m) * z ...
-             + pm.chi_I * z * phi(z + zE(i_q,i_m)) * (V0plus(i_q,i_m) - V0(i_q,i_m)) ...
-             + pm.chi_E * zE(i_q,i_m) * phi(z + zE(i_q,i_m)) * (-V0(i_q,i_m));
-         
-end
-
